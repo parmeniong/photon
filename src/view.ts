b7111695cd89a934cd9d371f74a1403d47737804
parmeniong <par.graikos@icloud.com>
2025-04-04 17:57:47 +0300

    Add View class

---
+++ src/view.ts
@@ -0,0 +1,15 @@
+import Element from "./element";
+
+class View {
+    elements: Array<Element>;
+
+    constructor(...elements: Array<Element>) {
+        this.elements = elements;
+    }
+
+    render(): string {
+        var result: string = "";
+        this.elements.forEach((element) => result += element.render());
+        return result;
+    }
+}
